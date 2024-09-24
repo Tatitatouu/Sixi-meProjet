7f61377cd07fbf2fd7935138de298b006bdb0b96
Tatitatouu <tatiana68270wi@gmail.com>
2024-09-24 10:20:30 +0200

    Ajout du script Login

---
+++ FrontEnd/scriptLogin.js
@@ -0,0 +1,36 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const form = document.querySelector('.form-login');
+    
+    form.addEventListener('submit', async function(event) {
+        event.preventDefault();
+
+        const email = document.querySelector('.email').value;
+        const password = document.querySelector('.password').value;
+
+        try {
+            const response = await fetch('http://localhost:5678/api/users/login', {
+                method: 'POST',
+                headers: {
+                    'Accept': '*/*',
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    email: email,
+                    password: password
+                })
+            });
+
+            if (!response.ok) {
+                throw new Error('Échec de la connexion');
+            }
+
+            const data = await response.json();
+
+            localStorage.setItem('token', data.token); 
+            window.location.href = 'index.html'; 
+            
+        } catch (error) {
+            alert('Erreur de connexion. Veuillez vérifier vos informations.');
+        }
+    });
+});
